c5a8eb5d7f246e58d297167470a28f1d66d1d64d
darkzone44 <123981910+darkzone44@users.noreply.github.com>
2025-12-06 08:00:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ st.set_page_config(
 )
 
 custom_css = """
-   <style>
+    <style>
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700;800&display=swap');
     
     * {
